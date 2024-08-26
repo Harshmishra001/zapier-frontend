530fe8cd1492f8b6f3519789f1e98f1742000d60
Harshmishra001 <iconharsh01@gmail.com>
2024-08-26 15:14:52 +0530

    zap

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,6 @@
-export const BACKEND_URL = "https://zapier-backend-nmx4.onrender.com"
-export const HOOKS_URL = "https://zapier-hooks.onrender.com"
+export const BACKEND_URL = "https://zapier-backend-alp7.onrender.com"
+export const HOOKS_URL = "https://zapier-hooks-kfvs.onrender.com"
+
+
+
+// agr env na bnaye tb hume  ye config.ts m use krlo vo dikkt ni ni deta
